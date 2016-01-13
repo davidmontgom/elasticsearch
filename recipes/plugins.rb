52df11d45454352b25317c97825e260cee86ff3d
David Montgomery <davidmontgomery@gmail.com>
2016-01-13 22:49:34 +0800

    f

--- recipes/plugins.rb
+++ recipes/plugins.rb
@@ -1,3 +1,5 @@
+
+=begin
 bash "elasticsearch-head" do
   user "root"
   cwd "/usr/share"
@@ -5,6 +7,22 @@ bash "elasticsearch-head" do
     sudo elasticsearch/bin/plugin -install mobz/elasticsearch-head
     sudo elasticsearch/bin/plugin -install elasticsearch/marvel/latest
     sudo elasticsearch/bin/plugin --install jettro/elasticsearch-gui 
+    touch /var/chef/cache/elasticsearch-head.lock
+  EOH
+  action :run
+  not_if {File.exists?("/var/chef/cache/elasticsearch-head.lock")}
+end
+=end
+
+
+
+bash "elasticsearch-head" do
+  user "root"
+  cwd "/usr/share"
+  code <<-EOH
+    cd /usr/share/elasticsearch
+    sudo bin/plugin install license
+    sudo bin/plugin install marvel-agent
     touch /var/chef/cache/elasticsearch-head.lock
   EOH
   action :run
