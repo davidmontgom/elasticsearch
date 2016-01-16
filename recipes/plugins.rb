2fdc01cfde5a65d567c699422a11ad8cfe6c735a
David Montgomery <davidmontgomery@gmail.com>
2016-01-16 09:32:18 +0800

    f

--- recipes/plugins.rb
+++ recipes/plugins.rb
@@ -14,9 +14,11 @@ bash "elasticsearch-head" do
 end
 =end
 
-#http://1-elasticsearch-do-development-ny-forex.forexhui.com:9200/_plugin/marvel/
+#http://1-elasticsearch-do-development-ny-forex.forexhui.com:5601/app/kibana
+#http://1-elasticsearch-do-development-ny-forex.forexhui.com:5601/app/marvel
 
-bash "elasticsearch-head" do
+
+bash "marvel_install" do
   user "root"
   cwd "/usr/share"
   code <<-EOH
@@ -28,3 +30,51 @@ bash "elasticsearch-head" do
   action :run
   not_if {File.exists?("/var/chef/cache/elasticsearch-head.lock")}
 end
+
+kibana_version = "4.3.1"
+bash "kibana_kibana" do
+  user "root"
+  cwd "/usr/share"
+  code <<-EOH
+    cd /var
+    wget https://download.elastic.co/kibana/kibana/kibana-#{kibana_version}-linux-x64.tar.gz
+    tar -xvf kibana-#{kibana_version}-linux-x64.tar.gz
+    cd kibana-#{kibana_version}-linux-x64
+    bin/kibana plugin --install elasticsearch/marvel/latest
+    touch /var/chef/cache/kibana.lock
+  EOH
+  action :run
+  not_if {File.exists?("/var/chef/cache/kibana.lock")}
+end
+
+
+
+template "/var/kibana-#{kibana_version}-linux-x64/config/kibana.yml" do
+    path "/var/kibana-#{kibana_version}-linux-x64/config/kibana.yml"
+    source "kibana.#{kibana_version}.yml.erb"
+    owner "root"
+    group "root"
+    mode "0755"
+    notifies :restart, resources(:service => "elasticsearch")
+end
+
+service "supervisord"
+template "/etc/supervisor/conf.d/supervisord.kibana.conf" do
+      path "/etc/supervisor/conf.d/supervisord.kibana.conf"
+      source "supervisord.kibana.conf.erb"
+      owner "root"
+      group "root"
+      mode "0755"
+      variables({
+        :kibana_version => kibana_version
+      })
+      notifies :restart, resources(:service => "supervisord")
+end
+
+
+
+
+
+
+
+
