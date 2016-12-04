83167e6af37cb8fa5708b0c04afc47c5b4c28d82
Your Name <you@example.com>
2016-12-04 16:31:35 +0800

    xpack

--- recipes/xpack.rb
+++ recipes/xpack.rb
@@ -2,25 +2,25 @@ server_type = node.name.split('-')[0]
 
 
 if server_type == 'elasticsearch'
-    bash 'elasticsearch_xpack' do
-        code <<-EOH
+  bash 'elasticsearch_xpack' do
+    code <<-EOH
         cd /usr/share/elasticsearch
         echo 'y' |  bin/elasticsearch-plugin install x-pack
         EOH
     action :run
     not_if {File.exists?("/usr/share/elasticsearch/plugins/x-pack")}
-    end
+  end
 end
 
 if server_type == 'kibana'
-    bash 'kibana_xpack' do
-        code <<-EOH
+  bash 'kibana_xpack' do
+    code <<-EOH
         cd /usr/share/kibana/
         echo 'y' | bin/kibana-plugin install x-pack
         EOH
     action :run
     not_if {File.exists?("/usr/share/kibana/plugins/x-pack")}
-    end
+  end
 end
 
 
